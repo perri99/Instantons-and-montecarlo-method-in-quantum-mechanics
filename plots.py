89f4683f1908d4bf8a95a3ca8abbe6b01fc7ab4c
perri99 <130299266+perri99@users.noreply.github.com>
2023-07-11 19:36:22 +0200

    Add files via upload

---
+++ plots.py
@@ -0,0 +1,545 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+n = 800
+a = 0.05
+
+#------------------------------------------------------------------------------
+#   FIG. 2: Typical euclidean path obtained in a Monte Carlo simulation of the 
+#   discretized euclidean action of the double well potential.
+#------------------------------------------------------------------------------
+''' Typical path vs cooled path '''
+with open('Data/qmcool/config2.dat', 'r') as file:
+    lines = file.readlines()
+
+start_line = None
+end_line   = None
+for i, line in enumerate(lines):
+    if line.startswith('configuration: 355'):
+        start_line = i
+        break
+data_lines = lines[start_line+1: start_line+801]
+
+column1 = [float(line.split()[0]) for line in data_lines]
+column2 = [float(line.split()[1]) for line in data_lines]
+ 
+x     = np.array(column1)
+y     = np.array(column2)
+plt.plot(x, y, color = 'black',linewidth = 0.8, label = 'Monte Carlo')
+
+with open('Data/qmcool/config_cool.dat', 'r') as file:
+    lines = file.readlines()
+
+start_line = None
+end_line   = None
+for i, line in enumerate(lines):
+    if line.startswith('configuration: 355'):
+        start_line = i
+        break
+data_lines = lines[start_line+1: start_line+801]
+
+column1 = [float(line.split()[0]) for line in data_lines]
+column2 = [float(line.split()[1]) for line in data_lines]
+ 
+x     = np.array(column1)
+y     = np.array(column2)
+plt.plot(x, y, color = 'red',linewidth = 1.7, label = 'cooling')
+plt.legend()
+plt.xlabel('time t')
+plt.ylabel('x(t)')
+plt.title('Typical configuration')
+plt.savefig('Data/qmcool/config.pdf')
+plt.savefig('Data/qmcool/config.png')
+plt.show()
+
+#---------Fig3: Probability distribution-----------------------------------|
+'''Exact groundstate vs Montecarlo groundstate'''
+
+with open('Data/qmdiag/ground_state1_doublewell.dat', 'r') as file:
+    lines = file.readlines()[1:1000]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+plt.plot(x, y**2, color = 'red', label = 'exact')
+
+with open('Data/qm/wavefunction_qm.dat', 'r') as file:
+    lines = file.readlines()[1:50]  # read lines 1 to 51
+    
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+plt.plot(x, y, color = 'blue', drawstyle = 'steps', label = 'Monte Carlo' )
+
+
+plt.xlabel('x')
+plt.ylabel('P(x)')
+plt.legend()
+plt.title('Probability distribution |\u03C8|\u00B2')
+plt.savefig('Data/qm/probability distribution.pdf')
+plt.savefig('Data/qm/probability distribution.png')
+plt.show()
+#------------------------------------------------------------------------------
+#   FIG. 4: Fig. a. Shows the correlation functions
+#------------------------------------------------------------------------------
+''' Exact correlation functions vs montecarlo '''
+
+with open('Data/qmdiag/cor.dat', 'r') as file:
+    lines = file.readlines()
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+
+
+x = np.array(column1)
+y = np.array(column2)
+plt.plot(x, y, color = 'black')
+
+with open('Data/qmdiag/cor2.dat', 'r') as file:
+    lines = file.readlines()
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+
+
+x = np.array(column1)
+y = np.array(column2)
+plt.plot(x, y, color = 'black')
+
+with open('Data/qmdiag/cor3.dat', 'r') as file:
+    lines = file.readlines()
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+
+
+x = np.array(column1)
+y = np.array(column2)
+plt.plot(x, y, color = 'black')
+
+
+with open('Data/qm/correlations_qm.dat', 'r') as file:
+    lines = file.readlines()[2:20]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+column3 = [float(line.split()[2]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+y_err = np.array(column3)
+plt.errorbar(x, y, yerr=y_err, fmt='s', markerfacecolor='none',
+             markeredgecolor = 'blue', markersize=8, capsize=5, label = '<x(0)x(τ)>')
+
+with open('Data/qm/correlations2_qm.dat', 'r') as file:
+    lines = file.readlines()[2:20]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+column3 = [float(line.split()[2]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+y_err = np.array(column3)
+plt.errorbar(x, y, yerr=y_err, fmt='s', markerfacecolor='none',
+             markeredgecolor = 'red', markersize=8, capsize=5, label = '<x^2(0)x^2(τ)>')
+
+with open('Data/qm/correlations3_qm.dat', 'r') as file:
+    lines = file.readlines()[2:20]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+column3 = [float(line.split()[2]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+y_err = np.array(column3)
+plt.errorbar(x, y, yerr=y_err, fmt='s', markerfacecolor='none',
+             markeredgecolor = 'green', markersize=8, capsize=5, label = '<x^3(0)x^3(τ)>')
+plt.legend()
+plt.title('Correlation functions')
+plt.xlim(0, 1.5)
+plt.xlabel('time')
+plt.ylabel('<x^n(0)x^n(τ)>')
+plt.savefig('Data/qm/correlations.pdf')
+plt.savefig('Data/qm/correlations.png')
+plt.show()
+#------------------------------------------------------------------------------
+#   FIG. 4: Fig. b. Shows the logarithmic derivative of the correlators
+#------------------------------------------------------------------------------
+''' Exact log derivatives vs Montecarlo simulation'''
+with open('Data/qmdiag/dlog.dat', 'r') as file:
+    lines = file.readlines()
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+column3 = [float(line.split()[2]) for line in lines]
+column4 = [float(line.split()[3]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+plt.plot(x, y, color = 'black')
+
+y = np.array(column3)
+plt.plot(x, y, color = 'black')
+
+y = np.array(column4)
+plt.plot(x, y, color = 'black')
+
+with open('Data/qm/correlations_qm.dat', 'r') as file:
+    lines = file.readlines()[2:20]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[3]) for line in lines]
+column3 = [float(line.split()[4]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+y_err = np.array(column3)
+plt.errorbar(x, y, yerr=y_err, fmt='s', markerfacecolor='none',
+             markeredgecolor = 'blue', markersize=8, capsize=5, label = 'dlog<x(0)x(τ)>')
+
+with open('Data/qm/correlations2_qm.dat', 'r') as file:
+    lines = file.readlines()[2:20]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[3]) for line in lines]
+column3 = [float(line.split()[4]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+y_err = np.array(column3)
+plt.errorbar(x, y, yerr=y_err, fmt='s', markerfacecolor='none',
+             markeredgecolor = 'red', markersize=8, capsize=5, label = 'dlog<x^2(0)x^2(τ)>')
+
+with open('Data/qm/correlations3_qm.dat', 'r') as file:
+    lines = file.readlines()[2:20]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[3]) for line in lines]
+column3 = [float(line.split()[4]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+y_err = np.array(column3)
+plt.errorbar(x, y, yerr=y_err, fmt='s', markerfacecolor='none',
+             markeredgecolor = 'green', markersize=8, capsize=5, label = 'dlog<x^2(0)x^2(τ)>')
+plt.legend()
+plt.ylim(0, 6)
+plt.xlim(0, 1)
+plt.title('Logarithmic derivatives of correlation functions')
+plt.xlabel('time')
+plt.ylabel('dlog<x^n(0)x^n(τ)>')
+plt.savefig('Data/qm/dlog.pdf')
+plt.savefig('Data/qm/dlog.png')
+plt.show()
+#------------------------------------------------------------------------------
+#   FIG. 5: Free energy F = −T log(Z) of the anharmonic oscillator as a 
+#   function of the temperature t = i/b
+#------------------------------------------------------------------------------
+with open('Data/qmdiag/partition_function.dat', 'r') as file:
+    lines = file.readlines()[1:]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[2]) for line in lines]
+
+x     = np.array(column1)
+y     = np.array(column2)
+
+plt.plot(x, y, color = 'black', label = 'exact')
+
+with open('Data/qmswitch/energies.dat', 'r') as file:
+    lines = file.readlines()[1:]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+column3 = [float(line.split()[2]) for line in lines]
+
+x     = np.array(column1)
+y     = np.array(column2)
+y_err  = np.array(column3)
+
+plt.errorbar(x, y, yerr=y_err, fmt='s', markerfacecolor='none',
+             markeredgecolor = 'blue', markersize=8, capsize=5, label = 'simulated')
+
+plt.xlabel('T')
+plt.ylabel('F')
+plt.title('Free energy of anharmonic oscillator')
+plt.xscale('log')
+plt.xlim(0.01,2.5)
+plt.ylim(-2.5, -1)
+plt.show()
+
+#------------------------------------------------------------------------------
+#   FIG. 6: Fig. a. Shows the cooled correlation functions
+#------------------------------------------------------------------------------
+''' Exact correlation functions vs cooled correlations'''
+
+with open('Data/qmdiag/cor.dat', 'r') as file:
+    lines = file.readlines()
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+
+
+x = np.array(column1)
+y = np.array(column2)
+plt.plot(x, y, color = 'black')
+
+with open('Data/qmdiag/cor2.dat', 'r') as file:
+    lines = file.readlines()
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+
+
+x = np.array(column1)
+y = np.array(column2)
+plt.plot(x, y, color = 'black')
+
+with open('Data/qmdiag/cor3.dat', 'r') as file:
+    lines = file.readlines()
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+
+
+x = np.array(column1)
+y = np.array(column2)
+plt.plot(x, y, color = 'black')
+
+
+with open('Data/qmcool/correlations_cool.dat', 'r') as file:
+    lines = file.readlines()[2:20]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+column3 = [float(line.split()[2]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+y_err = np.array(column3)
+plt.errorbar(x, y, yerr=y_err, fmt='s', markerfacecolor='none',
+             markeredgecolor = 'blue', markersize=8, capsize=5, label = '<x(0)x(τ)>')
+
+with open('Data/qmcool/correlations2_cool.dat', 'r') as file:
+    lines = file.readlines()[2:20]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+column3 = [float(line.split()[2]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+y_err = np.array(column3)
+plt.errorbar(x, y, yerr=y_err, fmt='s', markerfacecolor='none',
+             markeredgecolor = 'red', markersize=8, capsize=5, label = '<x^2(0)x^2(τ)>')
+
+with open('Data/qmcool/correlations3_cool.dat', 'r') as file:
+    lines = file.readlines()[2:20]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+column3 = [float(line.split()[2]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+y_err = np.array(column3)
+plt.errorbar(x, y, yerr=y_err, fmt='s', markerfacecolor='none',
+             markeredgecolor = 'green', markersize=8, capsize=5, label = '<x^3(0)x^3(τ)>')
+plt.legend()
+plt.title('Cooled correlation functions')
+plt.xlim(0, 1)
+plt.ylim(0,8)
+plt.xlabel('time')
+plt.ylabel('<x^n(0)x^n(τ)>')
+plt.savefig('Data/qmcool/correlations.pdf')
+plt.savefig('Data/qmcool/correlations.png')
+plt.show()
+
+#------------------------------------------------------------------------------
+#   FIG. 6: Fig. b. Shows the logarithmic derivative of the cooled correlators
+#------------------------------------------------------------------------------
+''' Exact log derivatives vs cooled ones'''
+with open('Data/qmdiag/dlog.dat', 'r') as file:
+    lines = file.readlines()
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+column3 = [float(line.split()[2]) for line in lines]
+column4 = [float(line.split()[3]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+plt.plot(x, y, color = 'black')
+
+y = np.array(column3)
+plt.plot(x, y, color = 'black')
+
+y = np.array(column4)
+plt.plot(x, y, color = 'black')
+
+with open('Data/qmcool/correlations_cool.dat', 'r') as file:
+    lines = file.readlines()[2:20]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[3]) for line in lines]
+column3 = [float(line.split()[4]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+y_err = np.array(column3)
+plt.errorbar(x, y, yerr=y_err, fmt='s', markerfacecolor='none',
+             markeredgecolor = 'blue', markersize=8, capsize=5, label = 'dlog<x(0)x(τ)>')
+
+with open('Data/qmcool/correlations2_cool.dat', 'r') as file:
+    lines = file.readlines()[2:20]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[3]) for line in lines]
+column3 = [float(line.split()[4]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+y_err = np.array(column3)
+plt.errorbar(x, y, yerr=y_err, fmt='s', markerfacecolor='none',
+             markeredgecolor = 'red', markersize=8, capsize=5, label = 'dlog<x^2(0)x^2(τ)>')
+
+with open('Data/qmcool/correlations3_cool.dat', 'r') as file:
+    lines = file.readlines()[2:20]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[3]) for line in lines]
+column3 = [float(line.split()[4]) for line in lines]
+
+x = np.array(column1)
+y = np.array(column2)
+y_err = np.array(column3)
+plt.errorbar(x, y, yerr=y_err, fmt='s', markerfacecolor='none',
+             markeredgecolor = 'green', markersize=8, capsize=5, label = 'dlog<x^2(0)x^2(τ)>')
+plt.legend()
+plt.ylim(-0.2, 6)
+plt.xlim(0, 1)
+plt.title('Logarithmic derivatives of cooled correlation functions')
+plt.xlabel('time')
+plt.ylabel('dlog<x^n(0)x^n(τ)>')
+plt.savefig('Data/qmcool/dlog.pdf')
+plt.savefig('Data/qmcool/dlog.png')
+plt.show()
+
+#------------------------------------------------------------------------------
+#   FIG. 7: Fig. a. instanton density as a function of the number of cooling 
+#   sweeps for different values of the parameter η
+#------------------------------------------------------------------------------
+with open('Data/qmcool/instdensity.dat', 'r') as file:
+    lines = file.readlines()[2:]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+column3 = [float(line.split()[3]) for line in lines]
+column4 = [float(line.split()[4]) for line in lines]
+x     = np.array(column1)
+y     = np.array(column2)/(n*a)
+
+
+plt.errorbar(x, y, fmt ='s', markerfacecolor = 'none',
+             markeredgecolor = 'blue', markersize = 8, capsize = 5, label = 'f = 1.4')
+
+y     = np.array(column3)/(n*a)
+plt.plot(x, y, color = 'green', linewidth = 0.8, linestyle = '--', label = '2-loop')
+
+y     = np.array(column4)/(n*a)
+plt.plot(x, y, color = 'green', linewidth = 0.8, label = '1-loop')
+
+plt.xlabel('n_cool')
+plt.ylabel('N_top/\u03B2')
+plt.title('Instanton density')
+
+plt.xscale('log')
+plt.yscale('log')
+plt.xlim(1, )
+plt.legend()
+plt.show()
+
+#------------------------------------------------------------------------------
+#   FIG. 7: Fig. b. instanton action as a function of the number of cooling 
+#   sweeps for different values of the parameter eta
+#------------------------------------------------------------------------------
+with open('Data/qmcool/inst_action.dat', 'r') as file:
+    lines = file.readlines()[2:]
+
+column1 = [float(line.split()[0]) for line in lines]
+column2 = [float(line.split()[1]) for line in lines]
+column3 = [float(line.split()[2]) for line in lines]
+column4 = [float(line.split()[3]) for line in lines]
+x     = np.array(column1)
+y     = np.array(column2)
+y_err = np.array(column3)
+
+plt.errorbar(x, y, yerr=y_err, fmt='s',markerfacecolor='none',
+             markeredgecolor = 'blue', markersize=8, capsize=5, label = 'η = 1.4')
+
+y     = np.array(column4)
+
+plt.plot(x, y, color='green', linewidth = 0.8, label= 'classical')
+
+plt.xlabel('n_cool')
+plt.ylabel('S/N_inst')
+plt.title('Action per instanton')
+
+plt.xscale('log')
+plt.yscale('log')
+plt.xlim(1, )
+plt.legend()
+plt.savefig('Data/qmcool/inst_action.pdf')
+plt.savefig('Data/qmcool/inst_action.png')
+plt.show()
+#------------------------------------------------------------------------------
+#   FIG. 9: Quantum mechanical paths which appear in a Monte-Carlo calculation
+#   of the one-instanton partition function in the double well potential.
+#------------------------------------------------------------------------------
+for i in range(4):
+    with open('Data/qmidens/idens_conf.dat', 'r') as file:
+        lines = file.readlines()[i*n+1:(i+1)*n]
+
+    column1 = [float(line.split()[0]) for line in lines]
+    column2 = [float(line.split()[1]) for line in lines]
+
+    x     = np.array(column1)
+    y     = np.array(column2)
+    if i==0:
+        plt.plot(x, y, color = 'black', linewidth = 0.8)
+    else:
+        plt.plot(x, y, color = 'green', linewidth = 0.8, label = 'instanton fluctuations')
+for i in range(4):
+    with open('Data/qmidens/vac_conf.dat', 'r') as file:
+        lines = file.readlines()[i*n+1:(i+1)*n]
+
+    column1 = [float(line.split()[0]) for line in lines]
+    column2 = [float(line.split()[1]) for line in lines]
+
+    x     = np.array(column1)
+    y     = np.array(column2)
+    if i==0:
+        plt.plot(x, y, color = 'black', linewidth = 0.8)
+    else:
+        plt.plot(x, y, color = 'blue', linewidth = 0.8, label = 'vacuum fluctuations')
+plt.legend()
+plt.xlabel('τ')
+plt.ylabel('X')
+plt.title('Quantum mechanical path of one-instanton partition function')
+plt.xlim(0, n*a-a)
+plt.savefig('Data/qmidens/path.pdf')
+plt.savefig('Data/qmidens/path.png')
+plt.show()
+
+
+
+
+
+
---
+++ qmswitch_final.py
@@ -0,0 +1,158 @@
+import numpy as np
+import functions as fn
+import random
+from tqdm import tqdm
+import os
+
+def setting_inputs():
+    f = 1.4 #minimum of the potential
+    #n = 800 #lattice points
+    a = 0.05 #lattice spacing
+    neq = 100 #number of equilibration sweeps
+    nmc = 10**4 #number of MonteCarlo sweeps
+    dx = 0.5 #width of updates
+    n_alpha = 20 #number of switch
+    nc = 5 #number of correlator measurements in a configuration
+    kp = 50 #number of sweeps between writeout of complete configuration 
+    mode = 1 # ih=0: cold start, x_i=-f; ih=1: hot start, x_i=random
+    seed = 597
+    w0 = 5.6
+    return f,  a, neq, nmc, dx, n_alpha, nc, kp, mode, seed, w0
+
+#setting inputs
+f, a, neq, nmc, dx, n_alpha, nc, kp, mode, seed, w0 = setting_inputs()
+random.seed(seed)
+
+#output files
+data_folder = 'Data'
+subfolder = 'qmswitch' 
+if not os.path.exists(data_folder):
+    os.makedirs(data_folder)
+folder_path = os.path.join(data_folder, subfolder)
+if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+energy = open('Data/qmswitch/energies.dat', 'w')
+energy.write('T\t E\n')        
+for index in range(6):
+    n = 800 - index *120
+    switch = open('Data/qmswitch/switch.dat','w')
+    switch.write('montecarlo switch\n ----- \n')
+    switch.write('f\t n\t a\t nmc\t neq\t dx\t mode\t w0\t n_alpha\n')
+    switch.write("{:.2f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\n\n".format(f,n,a, nmc, neq, dx, mode, w0, n_alpha))
+
+    #counters
+    nconf = 0
+    ncor  = 0
+
+
+    #definitions
+    dalpha = 1.0/float(n_alpha)
+    beta   = n*a
+    e0     = w0/2.0   #energy of ground state of harmonic oscillator
+    f0     = 1.0/beta*np.log(2.0*np.sinh(e0*beta)) #harmonic oscillator free energy
+    ei     = e0      
+    #-------variable definitions----------------------------------------|
+    S_sum = 0.0
+    S2_sum = 0.0
+    T_sum = 0.0
+    T2_sum = 0.0
+    V_sum = 0.0
+    V2_sum = 0.0
+    Valpha_sum = 0.0
+    Valpha2_sum = 0.0
+    x_sum = 0
+    x2_sum = 0
+    x4_sum = 0
+    x8_sum = 0
+    #-----Array defnitions-----------------------------------------------|
+    Va_av = np.zeros(2*n_alpha+1)
+    Va_err = np.zeros(2*n_alpha+1)
+    #starting configuration-----------------------------------------|
+    x = fn.periodic_starting_conf(n, f, mode)
+    S, V, P = fn.compute_energy_switch(x, n, a, f, w0, 0)
+    #--------loop over coupling constant alpha-------------------------------------|
+    for ialpha in tqdm(range(2 * n_alpha + 1)):
+        if ialpha <= n_alpha:
+            alpha = ialpha * dalpha  #da 0 a 1
+        else:
+            alpha = 2.0 - ialpha * dalpha #da 1 a 0
+        #---------montecarlo generations-------------------------------------------|
+        for i in range(nmc):
+            nconf += 1
+            if i == neq:  
+                nconf = 0#when we reach equilibrium all is set to zero
+                S_sum = 0.0
+                S2_sum = 0.0
+                T_sum = 0.0
+                T2_sum = 0.0
+                V_sum = 0.0
+                V2_sum = 0.0
+                Valpha_sum = 0.0
+                Valpha2_sum = 0.0
+                x_sum = 0
+                x2_sum = 0
+                x4_sum = 0
+                x8_sum = 0        
+            x = fn.update_periodic_switch(x,n,a,f, w0, alpha, dx)
+            #computation of action, kinetic energy, potential and virial for the current configuration
+            S, V, P = fn.compute_energy_switch(x, n, a, f, w0, alpha)
+            S_sum  += S
+            S2_sum += S**2
+            V_sum  += V/beta
+            V2_sum += V**2/beta
+            Valpha_sum  += P/beta
+            Valpha2_sum += P**2/beta
+            for k in range(n):
+                x_sum  += x[k]
+                x2_sum += x[k]**2
+                x4_sum += x[k]**4
+                x8_sum += x[k]**8
+        #----------end of montecarlo cicle-----------------------------------------|
+        stot_av, stot_err     = fn.dispersion(nconf, S_sum, S2_sum)
+        v_av, v_err           = fn.dispersion(nconf, V_sum, V2_sum)
+        valpha_av, valpha_err = fn.dispersion(nconf,Valpha_sum, Valpha2_sum)
+        x_av, x_err           = fn.dispersion(nconf*n, x_sum, x2_sum)
+        x2_av,x2_err          = fn.dispersion(nconf*n, x2_sum, x4_sum)
+        x4_av,x4_err          = fn.dispersion(nconf*n, x4_sum, x8_sum)
+        
+        Va_av[ialpha]  = valpha_av
+        Va_err[ialpha] = valpha_err
+        
+        if ialpha % (2 * n_alpha) == 0:
+            da = dalpha / 4.0
+        else:
+            da = dalpha / 2.0   #chiedere informazioni
+            
+        de = da * valpha_av
+        ei += de
+        #---------outputs----------------------------------------------------------|
+        switch.write('alpha\t stot_av\t stot_err\n')
+        switch.write("{:.2f}\t{:.4f}\t{:.4f}\n".format(alpha,stot_av,stot_err))
+        switch.write('x_av, x_err, x2_av, x2_err, x4_av, x4_err\n')
+        switch.write("{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\n".format( x_av, x_err, x2_av, x2_err, x4_av, x4_err))
+        switch.write('v_av\t v_err\t valpha_av\t valpha_err\n')
+        switch.write("{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\n".format( v_av, v_err, valpha_av, valpha_err))
+        switch.write('ei\t de\t e0\n')
+        switch.write("{:.4f}\t{:.4f}\t{:.4f}\n\n".format( ei, de, e0))
+    #----------------end of loops over coupling constant alpha---------------------|
+    #------have sum=1/2(up+down) and up = 1/2*f0+f1+...+1/2*fn, down=..------------|
+    eup_sum, eup_err, eup_hal, edw_sum, edw_err, edw_hal = fn.summing(n_alpha, dalpha, Va_av, Va_err)
+    # -------------------------uncertainties --------------------------------------|                                                                        
+    de     = eup_sum + edw_sum
+    ei     = e0 + de
+    de_err = np.sqrt(eup_err + edw_err)
+    de_hal = eup_hal + edw_hal
+    de_dif = abs(eup_sum - edw_sum)
+    de_dis = abs(de - de_hal)/2.0
+    de_tot = np.sqrt(de_err**2 + de_dif**2 + de_dis**2)
+    #outputs
+    switch.write('input parameters\n -----------\n')
+    switch.write('beta\t f0\t e0\n')
+    switch.write("{:.4f}\t{:.4f}\t{:.4f}\n\n".format( beta, f0, e0))
+    switch.write('final initial energy\n ei\t de\t e0\n')
+    switch.write("{:.4f}\t{:.4f}\t{:.4f}\n\n".format( ei, de, e0))
+    switch.write(' ei\t de_tot\t de_err\t de_diff\t de_dis\n')
+    switch.write("{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\n".format( ei, de_tot, de_err, de_dif, de_dis))
+    energy.write('{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}\t{:.4f}{:.4f}\n'.format(1/beta, -ei, de_tot, de_err, de_dif, de_dis))
+switch.close()
+energy.close()
